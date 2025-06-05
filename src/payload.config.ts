6cf9df0a710b7e6b32e293697347eb81fc20a326
vaish567 <vaishnav.parte@gmail.com>
2025-06-05 21:54:39 +0530

    update on links

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -14,7 +14,7 @@ import FAQs from './collections/FAQ'
 import Projects from './collections/Projects'
 import Testimonials from './collections/Testimonials'
 import Services from './collections/Services'
-
+import icon from '../public/favicon.ico'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
@@ -23,6 +23,21 @@ export default buildConfig({
     user: Users.slug,
     importMap: {
       baseDir: path.resolve(dirname),
+    },
+    // @ts-ignore - Ignore "unused attribute" warning
+    branding: {
+      companyName: 'ETP Builders Portal', // ✅ Keep only this if logo isn't needed
+    },
+
+    meta: {
+      titleSuffix: ' - ETP Portal',
+      icons: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          url: icon.src,
+        },
+      ],
     },
   },
   collections: [Users, Media, ContactUsForm, FAQs, Projects, Testimonials, Services],
