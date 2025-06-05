9403be16bafac3c9b29f1ddee47d7ed628eb5a4e
vaish567 <vaishnav.parte@gmail.com>
2025-06-05 22:00:16 +0530

    update on media.ts

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,16 +1,20 @@
 import type { CollectionConfig } from 'payload'
+import path from 'path'
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
   },
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: true,
-    },
-  ],
-  upload: true,
+  admin: {
+    hidden: true,
+  },
+  upload: {
+    staticDir: path.resolve('/var/www/etp-media'), // For linux
+    // staticDir: path.resolve('F:\Shawn\pbrs-uploads'), // For windows
+    // staticDir: 'media', // Folder where files will be stored
+    //  staticURL: '/media/images',
+    mimeTypes: ['image/*'], // Allow images & videos
+  },
+  fields: [],
 }
